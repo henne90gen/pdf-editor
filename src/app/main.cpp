c29372644ef19dd4a7a0e77f90b7c5970fd4b61f
Hendrik Müller <henne90gen@gmail.com>
2021-07-02 16:07:30 +0200

    Implemented tab view to show multiple pdf files

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,46 +3,60 @@
 #include <glibmm/convert.h>
 #include <gtkmm/applicationwindow.h>
 #include <gtkmm/builder.h>
+#include <gtkmm/notebook.h>
+#include <gtkmm/textview.h>
 
 class MainWindow : public Gtk::ApplicationWindow {
   public:
-    MainWindow(BaseObjectType *obj, Glib::RefPtr<Gtk::Builder> _builder)
+    [[maybe_unused]] MainWindow(BaseObjectType *obj, Glib::RefPtr<Gtk::Builder> _builder)
         : Gtk::ApplicationWindow(obj), builder(std::move(_builder)) {
-        targets = {Gtk::TargetEntry("text/uri-list")};
+
+        std::vector<Gtk::TargetEntry> targets = {Gtk::TargetEntry("text/uri-list")};
         this->drag_dest_set(targets, Gtk::DEST_DEFAULT_DROP | Gtk::DEST_DEFAULT_HIGHLIGHT | Gtk::DEST_DEFAULT_MOTION,
                             Gdk::ACTION_COPY | Gdk::ACTION_MOVE);
-        this->signal_drag_drop().connect(sigc::mem_fun(this, &MainWindow::on_drag_drop));
-        this->signal_drag_data_received().connect(sigc::mem_fun(this, &MainWindow::on_drag_data_received));
+        this->signal_drag_drop().connect(sigc::mem_fun(this, &MainWindow::custom_on_drag_drop));
+        this->signal_drag_data_received().connect(sigc::mem_fun(this, &MainWindow::custom_on_drag_data_received));
+
+        builder->get_widget("ContentNotebook", notebook);
+        for (int i = 0; i < notebook->get_n_pages(); i++) {
+            notebook->remove_page(i);
+        }
     }
 
     ~MainWindow() override = default;
 
   protected:
-    bool on_drag_drop(const Glib::RefPtr<Gdk::DragContext> &context, int x, int y, guint time) override {
+    bool custom_on_drag_drop(const Glib::RefPtr<Gdk::DragContext> &context, int x, int y, guint time) {
         // limits the data that is received to "text/uri-list", prevents on_drag_data_received from being triggered more
         // than once
         drag_get_data(context, "text/uri-list", time);
         return true;
     }
 
-    void on_drag_data_received(const Glib::RefPtr<Gdk::DragContext> &context, int x, int y,
-                               const Gtk::SelectionData &selection_data, guint info, guint time) override {
+    void custom_on_drag_data_received(const Glib::RefPtr<Gdk::DragContext> &context, int x, int y,
+                                      const Gtk::SelectionData &selection_data, guint info, guint time) {
         auto uriList = selection_data.get_uris();
         for (auto &uri : uriList) {
             auto fileName = Glib::filename_from_uri(uri);
-            std::cout << fileName << std::endl;
+
+            auto buffer = Gtk::TextBuffer::create();
+            buffer->set_text(fileName);
+            pages.emplace_back(buffer);
+            notebook->append_page(pages.back(), fileName);
         }
+        notebook->show_all();
         context->drag_finish(true, false, time);
     }
 
   private:
     Glib::RefPtr<Gtk::Builder> builder;
-    std::vector<Gtk::TargetEntry> targets = {};
+    Gtk::Notebook *notebook;
+    std::list<Gtk::TextView> pages;
 };
 
 int main(int argc, char *argv[]) {
     auto app     = Gtk::Application::create(argc, argv, "de.henne90gen.pdf_editor");
-    auto builder = Gtk::Builder::create_from_file("../../src/app/pdf-editor.glade");
+    auto builder = Gtk::Builder::create_from_file("../../../src/app/pdf-editor.glade");
 
     MainWindow *wnd = nullptr;
     builder->get_widget_derived("MainWindow", wnd);
