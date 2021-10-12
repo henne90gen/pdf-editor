af330e8b32290d4efef6af768214e7ac8bda5d0f
Hendrik Müller <henne90gen@gmail.com>
2021-10-12 21:22:08 +0200

    Improved output of integration tests

--- src/test/integration_test.py
+++ src/test/integration_test.py
@@ -13,16 +13,28 @@ def main():
     test_folder = sys.argv[2]
     for directory, b, files in os.walk(test_folder):
         for file in files:
+            if not file.endswith(".pdf"):
+                continue
+
             file_path = os.path.join(directory, file)
             if not os.path.exists(file_path):
                 print("☓", file_path, "does not exist")
 
+            print("->", file_path)
+
             process = subprocess.run([executable, "info", file_path], capture_output=True)
+
+            sys.stdout.write("\033[F")  # Cursor up one line
+            sys.stdout.write("\033[K")  # Clear to the end of line
             if process.returncode == 0:
                 print("✓", file_path)
             else:
                 print("☓", file_path)
-                print(process.stdout.decode("utf-8"))
+                print()
+                output = process.stdout.decode("utf-8")
+                parts = output.split("\n")
+                for part in parts:
+                    print("   ", part)
 
 
 if __name__ == "__main__":
