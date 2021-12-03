c412f546fcbe3091a6e8019daa502d1d8f374cb2
Hendrik Müller <henne90gen@gmail.com>
2021-12-03 14:59:00 +0100

    Made text move test more explicit

--- src/test/text_test.cpp
+++ src/test/text_test.cpp
@@ -35,14 +35,14 @@ TEST(Text, Move) {
         auto textBlocks = page->text_blocks();
         ASSERT_EQ(textBlocks.size(), 1);
 
-        textBlocks[0].move(document, 10, 10);
+        textBlocks[0].move(document, 10, 500);
     }
 
     char *buffer = nullptr;
     size_t size  = 0;
     ASSERT_FALSE(document.write_to_memory(buffer, size));
-    ASSERT_BUFFER_CONTAINS_AT(buffer, 19, "2 0 obj <<\n/Length 132\n/Filter /FlateDecode");
-    ASSERT_BUFFER_CONTAINS_AT(buffer, 6930, "0000000019 00000 n");
+    ASSERT_BUFFER_CONTAINS_AT(buffer, 19, "2 0 obj <<\n/Length 133\n/Filter /FlateDecode");
+    ASSERT_BUFFER_CONTAINS_AT(buffer, 6931, "0000000019 00000 n");
 
     pdf::Document newDocument;
     ASSERT_FALSE(pdf::Document::read_from_memory(buffer, size, newDocument));
@@ -57,7 +57,7 @@ TEST(Text, Move) {
 
         ASSERT_EQ(textBlocks[0].text, "Hello World");
         ASSERT_DOUBLE_EQ(textBlocks[0].x, 66.8);
-        ASSERT_DOUBLE_EQ(textBlocks[0].y, 77.900763779528006);
+        ASSERT_DOUBLE_EQ(textBlocks[0].y, 567.900763779528006);
         ASSERT_DOUBLE_EQ(textBlocks[0].width, 58.902000000000001);
         ASSERT_DOUBLE_EQ(textBlocks[0].height, 9);
     }
