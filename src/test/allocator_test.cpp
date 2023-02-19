de701e2aecde3ad6a9da979e58ea7a31dd9f2e39
Hendrik Müller <hendrik.mueller@devboost.com>
2023-02-19 19:29:22 +0100

    Fixed allocator test

--- src/test/allocator_test.cpp
+++ src/test/allocator_test.cpp
@@ -8,6 +8,7 @@ struct TestArena {
     uint8_t *bufferPosition    = nullptr;
     size_t virtualSizeInBytes  = 0;
     size_t reservedSizeInBytes = 0;
+    size_t pageSize            = 0;
 };
 
 TEST(Arena, can_handle_small_allocation) {
@@ -36,19 +37,19 @@ TEST(Arena, can_handle_growing_allocation) {
     ASSERT_EQ(sizeof(TestArena), sizeof(pdf::Arena));
 
     pdf::Arena arena = {};
-    const auto buf1  = arena.push(PAGE_SIZE);
+    const auto buf1  = arena.push(pdf::ARENA_PAGE_SIZE);
     ASSERT_TRUE(nullptr != buf1);
 
     const auto buf2 = arena.push(1);
     ASSERT_TRUE(nullptr != buf2);
 
     // returned allocations have adjacent addresses
-    ASSERT_EQ(buf1 + PAGE_SIZE, buf2);
+    ASSERT_EQ(buf1 + pdf::ARENA_PAGE_SIZE, buf2);
 
     const TestArena *testArena = (TestArena *)&arena;
     ASSERT_TRUE(nullptr != testArena->bufferPosition);
     ASSERT_TRUE(nullptr != testArena->bufferStart);
-    ASSERT_EQ(testArena->bufferStart + PAGE_SIZE + 1, testArena->bufferPosition);
+    ASSERT_EQ(testArena->bufferStart + pdf::ARENA_PAGE_SIZE + 1, testArena->bufferPosition);
     constexpr size_t MB = 1024 * 1024;
     constexpr size_t GB = 1024 * MB;
     ASSERT_EQ(64 * GB, testArena->virtualSizeInBytes);
